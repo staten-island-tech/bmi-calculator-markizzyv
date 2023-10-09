3f59bdba6a9c65ee9f8445432fa2d4c2e8011212
markizzyv <144455870+markizzyv@users.noreply.github.com>
2023-10-09 19:49:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,25 @@
 //declare and define function, input output machines
-function test(BMI){
-    console.log(BMI);
+function calculateBMI(){
+
 }
 //run the function
-test("BMI");
 
 
-
-//String ""
-const x = "String";
-//declaring a variable, defining a variable
-test(x);
-
-//Number/Integer
-const y = 98;
-test(y);
-
-//Boolean true or false
-const z = true;
-test(z);
-
-function square (x){
-    console.log(x * x)
+function BMI (x){
+    console.log((b / (a * a)) * 703)
 }
-
-function add (x,y){
-    console.log(x + y);
-} 
+ 
 
 //need to convert the String to a Number 
-let a = Number(prompt("Your Height (Feet'Inches)"));
+let a = Number(prompt("Your Height (Inches)"));
 let b = Number(prompt("Your Weight (Pounds)"));
 
-square (a)
 
-function init(){
-    y = weight;
-    z = height;
-    let x = getBMI(y, z);
-    check(x);
 
-} 
+BMI(a)
+BMI(b)
+
+
+
+
  
